9ef3634862da391be13938b5998841d510ce9a17
SeoMiYoung <seo20seo00@naver.com>
2025-01-05 01:06:17 +0900

    함수의 타입 지정

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-// type을 변수에 담아서 쓸 수 있음: Type alias
-// 보통 타입 작명 시, 대문자로 작명함
-type Name = string | number;
-
-let 이름 :Name = 'kim'; // string 또는 number
+function 함수(x :number) :number{
+    // 이 함수는 파라미터로 number
+    // return 값으로 number
+    return x * 2
+}
