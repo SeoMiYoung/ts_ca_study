f7c9312982299a93e1a84c4b2ac84cff3b593019
SeoMiYoung <seo20seo00@naver.com>
2025-01-05 01:11:24 +0900

    object에 타입 지정해야할 속성이 너무 많으면

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
-type Member = [number, boolean];
-let john:Member = [123, true]
-// [참고] 튜플 타입: https://velog.io/@from_numpy/TypeScript-Tuple%ED%8A%9C%ED%94%8C
+type Member = {
+    [key :string] : string
+    // 글자로 된 모든 object 속성의 타입은 string
+}
+let john : Member = { name : 'kim' }
