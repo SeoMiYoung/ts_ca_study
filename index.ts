3e4967b8a76a7ffdabc792acb1d154da03dc00cc
SeoMiYoung <seo20seo00@naver.com>
2025-01-05 01:04:23 +0900

    Type alias (타입을 변수에 담아서 쓰다)

--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
-let 이름:string | number = 'kim'; // string 또는 number
+// type을 변수에 담아서 쓸 수 있음: Type alias
+// 보통 타입 작명 시, 대문자로 작명함
+type Name = string | number;
+
+let 이름 :Name = 'kim'; // string 또는 number
