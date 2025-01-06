4258e8e8141a60512b22375d68e29284f95d5992
SeoMiYoung <seo20seo00@naver.com>
2025-01-06 12:28:58 +0900

    타입스크립트 기본 타입 정리 (primitive types)

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// var 이름 = 'kim';
+var 이름 = 'kim';
+var 나이 = 50;
+var 결혼했니 = undefined;
+var 회원들 = ['kim', 'park'];
+var 회원들2 = {
+    member1: 'kim',
+    member2: 'park',
+};
+// ts는 자동으로 타입을 씌워줍니다. 그렇게 동작하게 되어있습니다.
--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
-class User {
-    name :string;
-    constructor(name : string) {
-        this.name = name;
-    }
+let 이름 :string = 'kim';
+let 나이 :number = 50;
+let 결혼했니 :undefined = undefined;
+let 회원들 :string[] = ['kim', 'park'];
+let 회원들2 :{ member1 : string, member2 : string } = { 
+    member1 : 'kim',
+    member2 : 'park',
 }
+
+// ts는 자동으로 타입을 씌워줍니다. 그렇게 동작하게 되어있습니다.
+
