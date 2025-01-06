d444a11bb621344aa8ab962ed02628051fc496f6
SeoMiYoung <seo20seo00@naver.com>
2025-01-07 00:27:58 +0900

    함수에서 void 타입

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-function 함수(x :number) :number {
-    return x * 2
+function 함수(x :number) :void {
+    // void로 반환값 없는것에 대한 단호하게 타입 지정 가능
+    // 그냥 아예 :void이런거 안써도 됨됨
+    return x * 2 // return하는것에 대해 에러발생 
 }
 
-함수(30)
-함수('6') 
