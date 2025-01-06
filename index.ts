e4450adf7eb910e2d2c272c6550e57359cdafed0
SeoMiYoung <seo20seo00@naver.com>
2025-01-07 00:26:33 +0900

    함수에 타입 지정

--- index.ts
+++ index.ts
@@ -1,18 +1,6 @@
-// [숙제1] 다음 변수 4개에 타입을 지정해봅시다.
-let user :string= 'kim';
-let age :undefined | number = undefined;
-let married :boolean = false;
-let 철수 :(string | undefined | number | boolean)[] = [user, age, married];
-
-// [숙제2] 학교라는 변수에 타입지정해보십시오.
-let 학교 :{
-    score : (number | boolean)[],
-    teacher : string,
-    friend : string[] | string,
-} = {
-    score : [100, 97, 84],
-    teacher : 'Phil',
-    friend : 'John'
+function 함수(x :number) :number {
+    return x * 2
 }
-학교.score[4] = false;
-학교.friend = ['Lee', 학교.teacher]
+
+함수(30)
+함수('6') 
