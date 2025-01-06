ffd455cc49bf5fa096d45b23a093435bfb70389f
SeoMiYoung <seo20seo00@naver.com>
2025-01-06 23:35:21 +0900

    Union Type

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-var 이름 = 'kim';
-var 나이 = 50;
-var 결혼했니 = undefined;
-var 회원들 = ['kim', 'park'];
-var 회원들2 = {
-    member1: 'kim',
-    member2: 'park',
-};
-// ts는 자동으로 타입을 씌워줍니다. 그렇게 동작하게 되어있습니다.
-// [실습]
-// Q1. 여러분의 이름, 나이, 출생지역을 변수로 각각 저장해봅시다.
-var Name = "SeoMiYoung";
-var Age = 26;
-var BornArea = "Seoul";
-// Q2. 여러분이 가장 좋아하는 곡과 가수이름을 변수에 object 자료형으로 담아보십시오.
-var Favorite = {
-    Title: "Home",
-    Singer: "NMIXX"
-};
-// Q3. 자료의 타입지정을 해보도록 합시다.
-var project = {
-    member: ['kim', 'park'],
-    days: 30,
-    started: true,
-};
+var 회원 = 123; // union type(타입2개 이상 합친 새로운 타입만들기)
+var 회원들 = [1, '2', 3];
+var 오브젝트 = { a: '123' };
--- index.ts
+++ index.ts
@@ -1,33 +1,3 @@
-let 이름 :string = 'kim';
-let 나이 :number = 50;
-let 결혼했니 :undefined = undefined;
-let 회원들 :string[] = ['kim', 'park'];
-let 회원들2 :{ member1 : string, member2 : string } = { 
-    member1 : 'kim',
-    member2 : 'park',
-}
-
-// ts는 자동으로 타입을 씌워줍니다. 그렇게 동작하게 되어있습니다.
-
-// [실습]
-// Q1. 여러분의 이름, 나이, 출생지역을 변수로 각각 저장해봅시다.
-let Name :string = "SeoMiYoung";
-let Age :number = 26;
-let BornArea :string = "Seoul";
-
-// Q2. 여러분이 가장 좋아하는 곡과 가수이름을 변수에 object 자료형으로 담아보십시오.
-let Favorite :{ Title : string, Singer : string } = {
-    Title : "Home",
-    Singer : "NMIXX"
-}
-
-// Q3. 자료의 타입지정을 해보도록 합시다.
-let project :{ 
-    member : string[], 
-    days : number, 
-    started : boolean, 
-}= {
-    member : ['kim', 'park'],
-    days : 30,
-    started : true,
-}
+let 회원 :(number | string | boolean) = 123; // union type(타입2개 이상 합친 새로운 타입만들기)
+let 회원들 :(number | string)[] = [1, '2', 3];
+let 오브젝트 :{ a : string | number } = { a : '123'};
