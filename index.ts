3154a9ac58e07834b2f5f30bb842a71375fad272
SeoMiYoung <seo20seo00@naver.com>
2025-01-07 16:07:54 +0900

    type alias extend (|와 &)

--- index.ts
+++ index.ts
@@ -1,25 +1,15 @@
-// type alias (타입 변수)
-// 타입지정이 너무 길고 복잡하면 따로 저장해서 사용
-// 작명시, 영어대문자로 시작하는게 국룰!
-// 뒤에 Type을 붙여주면 더 명시적으로 표현 가능!
-type AnimalType = string | number | undefined;
+// object를 extend 한다 == 합친다 
+// 합치기(1)
+type Name = string;
+type Age = number;
+type Person = Name | Age; // union type으로 만듦
 
-let 동물 :AnimalType = 'kim';
-
-// <const 변수>
-const 출생지역 = { region : 'Seoul' }
-
-// 잠깐! const는 재할당을 막을뿐, 내부 수정은 막지 않음
-출생지역.region = 'Busan'; // 쌉가능 
-
-type GirlFriendType = {
-    readonly name : string
+// 합치기(2)
+type PositionX = {
+    x : number,
 }
-const 여친 :GirlFriendType = {
-    name : '엠버'
+type PositionY = {
+    y : number,
 }
-// 만약에 막고 싶으면? readonly 
-여친.name = '유라' // readonly로 인해 수정 불가 
-// 그치만, ts자체가 그냥 에러 표시 용도지, 실제 변환된 js파일은 에러 없고, 수정해줌
-
+type NewType = PositionX & PositionY; // { x : number, y : number } 이렇게 합쳐짐
 
