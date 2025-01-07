e7e3608104868e825a406fd0b07a83a53a8a12b0
SeoMiYoung <seo20seo00@naver.com>
2025-01-07 16:10:06 +0900

    같은 이름의 type 변수 재정의 불가능

--- index.ts
+++ index.ts
@@ -13,3 +13,6 @@ type PositionY = {
 }
 type NewType = PositionX & PositionY; // { x : number, y : number } 이렇게 합쳐짐
 
+// [참고]
+// 같은 이름의 type 변수 재정의 불가능
+type PositionX = string; // Duplicate identifier 'PositionX'에러 발생!
