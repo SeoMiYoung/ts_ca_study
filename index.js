e5b99e4bb7fd9b32300e6c6de3582a568e2e0e9b
SeoMiYoung <seo20seo00@naver.com>
2025-01-08 16:50:42 +0900

    HTML 조작시 narrowing 하는 방법4: optional chaining

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var 제목 = document.querySelector('#title');
-제목.innerHTML = '반가워요';
-// as는 자주쓰는거 추천X
-// 사실 as는 ts를 쓰는 이유가 거의 없음
-// 타입이 확실할 때 쓰길..
+if (제목 === null || 제목 === void 0 ? void 0 : 제목.innerHTML) { // 조건: 왼쪽의 innerHTML이 있으면 출력을 해주고, 없으면 undefined 뱉으셈 (optional chaining)
+    제목.innerHTML = '반가워요';
+    // 만약에 undefined가 반환될 경우 false로 간주되서 if문이 실행되지 않음
+}
