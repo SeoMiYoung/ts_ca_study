93527dde56da721476a4636875c070d5210fcd94
SeoMiYoung <seo20seo00@naver.com>
2025-01-08 16:43:40 +0900

    HTML 조작시 narrowing 하는 방법1: 일반 narrowing

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 let 제목 = document.querySelector('#title');
-제목.innerHTML = '반가워요';
+if (제목 != null) {
+    // 참고로 typeof는 "object", "string"등의 기본 자료형만 확인합니다. 따라서 "Element"이런 확인은 ㄴㄴ
+    // 만약에 DOM 객체인지 확인하려면 instanceof를 사용하세요. 
+    제목.innerHTML = '반가워요';
+}
 
 // 이 상태로 하면 에러가 발생함
 /*
