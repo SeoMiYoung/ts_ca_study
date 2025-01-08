d71046182145c96eb7432fa021b75c1e995d1478
SeoMiYoung <seo20seo00@naver.com>
2025-01-08 16:47:32 +0900

    HTML 조작시 narrowing 하는 방법3: as(type assertion)로 사기치기

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 var 제목 = document.querySelector('#title');
-if (제목 instanceof Element) {
-    // 참고로 typeof는 "object", "string"등의 기본 자료형만 확인합니다. 따라서 "Element"이런 확인은 ㄴㄴ
-    // 만약에 DOM 객체인지 확인하려면 instanceof를 사용하세요. 
-    제목.innerHTML = '반가워요';
-}
-// 이 상태로 하면 에러가 발생함
-/*
-'제목' is possibly 'null'.ts(18047)
-let 제목: Element | null
-*/
-// --> 이런 에러! 따라서 여러분은 타입 확정을 시켜줘야함! narrowing을 통해서! 
-// 왜 그렇냐면 document.querySelector('#title')를 제대로 찾지 못하면, null이 반환되기 때문임!
+제목.innerHTML = '반가워요';
+// as는 자주쓰는거 추천X
+// 사실 as는 ts를 쓰는 이유가 거의 없음
+// 타입이 확실할 때 쓰길..
