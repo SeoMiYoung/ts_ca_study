09bc665c4d8232ba930689732717d2018e9a8ba9
SeoMiYoung <seo20seo00@naver.com>
2025-01-08 18:30:14 +0900

    [숙제2] 타입스크립트로 HTML 변경과 조작할 때 주의점

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ html 안에 test.jpg를 보여주고 있는 이미지 태그가 있다고 칩시
 성공여부는 크롬 개발자도구 켜면 src 속성이 잘 바뀌었는지 확인가능하겠죠?
 */
 var 버튼 = document.querySelector('#button');
+var linkList = document.querySelectorAll(".naver");
 var testImg = document.querySelector('#image');
 if (testImg instanceof HTMLImageElement) {
     버튼 === null || 버튼 === void 0 ? void 0 : 버튼.addEventListener('click', function () {
@@ -15,3 +16,24 @@ if (testImg instanceof HTMLImageElement) {
         // 하지만, typescript는 버튼이 올바른 타입(HTMLButtonElement)임을 확인하지 않은 경우 경고를 띄울 수 있습니다.
     });
 }
+// querySelectorAll() 셀렉터를 쓰면 많은 요소를 한번에 찾을 수 있습니다.
+/*
+근데 이 경우 타입이 NodeListOf<어쩌구> 이렇게 나오는데 그냥 여러개 찾으면 이런 타입이 된다고 생각하면 됩니다.
+그리고 forEach 반복문을 이용해서 찾은 요소마다 각각 href 속성을 변경해줬다고 합니다.
+*/
+linkList.forEach(function (i) {
+    if (i instanceof HTMLAnchorElement) {
+        i.href = 'https://kakao.com';
+    }
+});
+/*
+<또 다른 답안>
+let 링크 = document.querySelectorAll('.naver');
+
+for (let i = 0; i < 3; i++){
+  let a = 링크[i];
+  if (a instanceof HTMLAnchorElement){
+    a.href = 'https://kakao.com'
+  }
+}
+*/ 
