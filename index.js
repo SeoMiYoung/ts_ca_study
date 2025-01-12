4de69ccc93d7b7761c4d83b979856f352c4588b1
SeoMiYoung <seo20seo00@naver.com>
2025-01-12 19:30:38 +0900

    constructor 파라미터 타입지정 가능

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
-/*
-[숙제1]
-html 안에 test.jpg를 보여주고 있는 이미지 태그가 있다고 칩시다.
-이미지를 new.jpg 라는 이미지로 바꾸고 싶으면 자바스크립트 코드를 어떻게 짜야할까요?
-성공여부는 크롬 개발자도구 켜면 src 속성이 잘 바뀌었는지 확인가능하겠죠?
-*/
-var 버튼 = document.querySelector('#button');
-var linkList = document.querySelectorAll(".naver");
-var testImg = document.querySelector('#image');
-if (testImg instanceof HTMLImageElement) {
-    버튼 === null || 버튼 === void 0 ? void 0 : 버튼.addEventListener('click', function () {
-        // ?. 이것도 narrowing으로 인정해줌
-        // 버튼에 addEventListener 가능하면 해주시고, 아니면 undefined 뱉으셈
-        testImg.src = "new.jpg";
-        // Optional Chaining은 안전하게 null을 처리합니다. 만약 버튼이 null이면, addEventListener는 실행되지 않으므로 런타임 에러는 발생하지 않습니다.
-        // 하지만, typescript는 버튼이 올바른 타입(HTMLButtonElement)임을 확인하지 않은 경우 경고를 띄울 수 있습니다.
-    });
-}
-// querySelectorAll() 셀렉터를 쓰면 많은 요소를 한번에 찾을 수 있습니다.
-/*
-근데 이 경우 타입이 NodeListOf<어쩌구> 이렇게 나오는데 그냥 여러개 찾으면 이런 타입이 된다고 생각하면 됩니다.
-그리고 forEach 반복문을 이용해서 찾은 요소마다 각각 href 속성을 변경해줬다고 합니다.
-*/
-linkList.forEach(function (i) {
-    if (i instanceof HTMLAnchorElement) {
-        i.href = 'https://kakao.com';
+var Person = /** @class */ (function () {
+    // ts랑 js의 차이점
+    //// Typescript의 constructor()는 필드값에 어쩌구가 미리 있어야 this.어쩌구 가능! 
+    function Person(a) {
+        this.data = 0; // 필드값
+        // 뭐.. return값에 대한 타입지정까지 할 필요는 X
+        // 복제되는 게 항상 object인데, return 타입지정할 이유는 없음 
+        this.name = a;
     }
-});
-/*
-<또 다른 답안>
-let 링크 = document.querySelectorAll('.naver');
-
-for (let i = 0; i < 3; i++){
-  let a = 링크[i];
-  if (a instanceof HTMLAnchorElement){
-    a.href = 'https://kakao.com'
-  }
-}
-*/ 
+    return Person;
+}());
+var 사람1 = new Person('young');
+var 사람2 = new Person('won');
+console.log(사람1.name);
+console.log(사람2.name);
--- index.ts
+++ index.ts
@@ -3,13 +3,15 @@ class Person {
   name :string; // name이라는 필드값을 미리 지정해줘야함 
   // ts랑 js의 차이점
   //// Typescript의 constructor()는 필드값에 어쩌구가 미리 있어야 this.어쩌구 가능! 
-  constructor() {
-    this.name = 'kim'
+  constructor(a :string) {
+    // 뭐.. return값에 대한 타입지정까지 할 필요는 X
+    // 복제되는 게 항상 object인데, return 타입지정할 이유는 없음 
+    this.name = a;
   }
 }
 
-let 사람1 = new Person();
-let 사람2 = new Person();
+let 사람1 = new Person('young');
+let 사람2 = new Person('won');
 
-사람1.data  // 이건 0을 의미함
-사람2.data  // 이것도 0을 의미함
+console.log(사람1.name);
+console.log(사람2.name);
