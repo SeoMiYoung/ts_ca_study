e9aaafad39591a582c03caa57919d77f22deedcf
SeoMiYoung <seo20seo00@naver.com>
2025-01-12 19:35:18 +0900

    prototype 타입 지정

--- index.js
+++ index.js
@@ -7,9 +7,17 @@ var Person = /** @class */ (function () {
         // 복제되는 게 항상 object인데, return 타입지정할 이유는 없음 
         this.name = a;
     }
+    // prototype 함수 타입 지정해서 쓰셈 
+    // 이게 바로 Person.prototype.함수1()
+    Person.prototype.함수1 = function (a) {
+        // 얘는 return이 있다면, return type까지 지정해주는게 좋긴 함 
+        console.log('안녕' + a);
+    };
     return Person;
 }());
 var 사람1 = new Person('young');
 var 사람2 = new Person('won');
 console.log(사람1.name);
 console.log(사람2.name);
+사람1.함수1('hello');
+사람2.함수1('hi');
